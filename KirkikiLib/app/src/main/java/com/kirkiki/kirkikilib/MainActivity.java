db4c3a72908d2e3fe286f3db44266e2ef791f8e6
huseyinbarin <developer.barin@gmail.com>
2017-10-20 11:45:48 +0300

    bug-fix

--- KirkikiLib/app/src/main/java/com/kirkiki/kirkikilib/MainActivity.java
+++ KirkikiLib/app/src/main/java/com/kirkiki/kirkikilib/MainActivity.java
@@ -2,11 +2,23 @@ package com.kirkiki.kirkikilib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import com.kirkiki.utillib.DeviceUtil;
+import java.net.SocketException;
+import org.pcc.webviewOverlay.WebViewOverlay;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    try {
+      Toast.makeText(this, DeviceUtil.getMacAddress(), Toast.LENGTH_SHORT).show();
+    } catch (SocketException e) {
+      Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+    }
+
+    WebViewOverlay webViewOverlay=new WebViewOverlay(this);
   }
 }
